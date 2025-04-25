416d3b30fdf72a1e3fa449adee665e7d9b1fce10
Adm-GTBenz <adamthimou1602@gmail.com>
2025-04-25 20:39:13 +0200

    c bon

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React from 'react';
 
 function App() {
-  return (
-    <div>
-      <h1>Bienvenue sur mon site !</h1>
-    </div>
+  return React.createElement(
+    'div',
+    { style: { textAlign: 'center', backgroundColor: 'rgb(255, 182, 193)' } },
+    React.createElement(
+      'a',
+      {
+        href: '2eme.html',
+        style: { display: 'block', fontSize: '24px', marginTop: '20%', color: 'rgb(0, 174, 255)' }
+      },
+      'Safae mi amor'
+    ),
+    React.createElement(
+      'div',
+      { style: { fontSize: '48px', marginTop: '10px' } },
+      '🥰😍❤️‍🩹🥺'
+    )
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -1,11 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
+import ReactDOM from 'react-dom';
+import './index.css';
 import App from './App';
-import './index.css'; // si tu veux ajouter du style
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+  </React.StrictMode>,
+  document.getElementById('root') // Point d'entrée pour React
 );
